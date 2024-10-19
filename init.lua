83e741d9b91c7a27690ab590fd34c9de5c9268bf
Diego Azulay <diego.azulay@mailbox.org>
2024-10-19 14:52:26 +0200

    Add ols

--- init.lua
+++ init.lua
@@ -550,7 +550,13 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        ols = {
+          init_options = {
+            enable_format = false,
+          },
+        },
         clangd = {},
+        -- hls = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
